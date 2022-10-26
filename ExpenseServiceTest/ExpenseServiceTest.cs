1123570c7cdcdbff585667389d4619854e887f66
zwang <zwang91@slb.com>
2022-10-26 13:38:34 +0800

    test: [200~        public void Should_return_internal_expense_type_if_project_is_internal()

--- ExpenseServiceTest/ExpenseServiceTest.cs
+++ ExpenseServiceTest/ExpenseServiceTest.cs
@@ -1,4 +1,6 @@
 using Expense.Service;
+using Expense.Service.Expense;
+using Expense.Service.Projects;
 using Xunit;
 
 namespace Expense.Service.Test
@@ -9,8 +11,11 @@ namespace Expense.Service.Test
         public void Should_return_internal_expense_type_if_project_is_internal()
         {
             // given
+            Project project = new Project(ProjectType.INTERNAL, "Internal project");
             // when
+            ExpenseType expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
             // then
+            Assert.Equal(ExpenseType.INTERNAL_PROJECT_EXPENSE, expenseType);
         }
 
         [Fact]
